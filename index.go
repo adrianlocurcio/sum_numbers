9a01431d7e7798f159a78a775c277614721fdc9d
Adrián Locurcio <adrianlocurcio@MacBook-Pro-de-Admin.local>
2024-08-28 18:56:44 -0300

    Added more godoc comments.

--- index.go
+++ index.go
@@ -1,5 +1,8 @@
+// This package aims to help developers to sum 2 numbers.
+// Please contact adrianlocurcio95@gmail.com if you need custom support for it.
 package sum_numbers
 
+// Sum 2 numbers and return the result
 func Add(a, b int) int {
 	return a + b
 }
